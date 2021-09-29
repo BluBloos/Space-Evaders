15052aef36666f91830d118cc812cd7f5ee98621
BluBloos <cnoah1705@gmail.com>
2021-09-29 15:06:43 -0400

    Hello World Raylib

---
+++ engine.cpp
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    
+    InitWindow(screenWidth, screenHeight, "Space Evaders");
+    SetTargetFPS(60);
+    
+    
+    // Main engine loop
+    // WindowShouldClose detects window close button or ESC key, things like this.
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        
+        DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+        
+        EndDrawing();
+    }
+    
+    // Close window and OpenGL context
+    CloseWindow();
+    
+    return 0;
+}
