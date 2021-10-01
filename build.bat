82ea05f937c53ec6b8342a4f9c2fb5532052fc08
BluBloos <cnoah1705@gmail.com>
2021-10-01 14:41:23 -0400

    Changed build.bat to make bin directory

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ pushd raylib\src
 mingw32-make PLATFORM=PLATFORM_DESKTOP
 popd
 
+mkdir bin
 pushd bin
 g++ ../src/engine.cpp -o SpaceEvaders.exe -I ../src/ -I ../raylib/src/ -ggdb -L ../raylib/src/ -lraylib -lopengl32 -lgdi32 -lwinmm  
 popd
