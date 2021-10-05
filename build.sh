827e12df655c395074c45a4e41283e4aaab90714
BluBloos <cnoah1705@gmail.com>
2021-10-05 19:40:06 -0400

    Build script for mac

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd raylib\src
+make PLATFORM=PLATFORM_DESKTOP
+popd
+
+mkdir bin
+pushd bin
+g++ ../src/engine.cpp -o SpaceEvaders.exe -I ../src/ -I ../raylib/src/ -ggdb -L ../raylib/src/ -lraylib -lopengl32 -lgdi32 -lwinmm  
+popd
