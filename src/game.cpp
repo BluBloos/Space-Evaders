b143c7ec5b2005c6d595aab14b91314ccacb65e7
BluBloos <cnoah1705@gmail.com>
2021-11-02 20:03:26 -0400

    Creation of Enemy Class

--- src/game.cpp
+++ src/game.cpp
@@ -12,11 +12,13 @@ Game::Game() {
     std::cout << "Game has been initialized\n\n";
     // Create game entities.
     this->characters.push_back(new Player((Vector2){500.0f, 100.0f}, 0));
-    this->enemies.push_back(new Enemy((Vector2){700.0f, 200.0f}, 0));
+    Enemy *evil_enemy = new Enemy((Vector2){700.0f, 200.0f}, 0);
+    evil_enemy->SetMovable(true, 0, -1, 100, 100);
     this->grounds.push_back(new Ground((Vector2){-20.0f, 400.0f}, 0, 1000, 200));
+    this->enemies.push_back((Entity *)evil_enemy);
     // Add a moveable platform for player to jump onto!
     Ground *epic_ground = new Ground((Vector2){400.0f, 200.0f}, 0, 200, 100);
-    epic_ground->SetMovable(true, 0, -1, 100, 100);
+    epic_ground->SetMovable(true, -1, 0, 100, 100);
     //epic_ground->SetOscillation(); // set the platform to go back and forth in movement
     this->grounds.push_back((Entity *)epic_ground);
 }
@@ -67,6 +69,12 @@ Game::~Game() {
 	        Entity *entity = this->grounds[i];
 	        delete entity;
 	}
+
+    for (unsigned int i = 0; i < this->enemies.size(); i++) {
+            Entity *entity = this->enemies[i];
+	        delete entity;
+    }
+
     for (unsigned int i = 0; i < this->characters.size(); i++) {
         Entity *entity = this->characters[i];
         delete entity; // NOTE(Noah): Pretty sure this works...
